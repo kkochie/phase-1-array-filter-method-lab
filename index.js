66053c78efb2f396f506ab5ce4729c631e578c4c
kkochie <kristinekochie@gmail.com>
2022-06-25 21:45:17 -0400

    commit tree 4bde3b867419c585af40877dede07a12d91d794b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter( (possibleMatch) => possibleMatch.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, firstThree) {
+        return drivers.filter( (name) => 
+            name.toLowerCase().indexOf(firstThree.toLowerCase()) === 0
+        );
+}
+
+function matchName(drivers, name) {
+    return drivers.filter((record) => record.name === name
+    )
+}
